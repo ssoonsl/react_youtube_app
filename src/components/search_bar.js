ad306113e3c777fc2f653d21689692b7a40aec9c
ssoonsl <ssoonsl616+gh@gmail.com>
2017-11-12 10:20:57 +0800

    017 Change component to be controlled

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: ''
+    }
   }
 
-  onInputChange() {
-
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 import SearchBar from './components/search_bar'
 
-const API_KEY 
+const API_KEY = ''
 
 const App = () => {
   return (
